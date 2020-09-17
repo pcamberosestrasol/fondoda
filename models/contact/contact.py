b52903a6573040922e4a2bff614d0da6df973df2
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-09-17 10:20:51 -0500

    cambio

--- models/contact/contact.py
+++ models/contact/contact.py
@@ -28,7 +28,7 @@ class FondoContact(models.Model):
     benef_birth = fields.Date('Fecha de nacimiento del beneficiario',tracking=True)
     benef_phone = fields.Char('Número telefónico del beneficiario',tracking=True)
     is_colaborator = fields.Boolean('Colaborador',compute='define_type_of_user',store=True,)
-
+    prestamos_id = fields.One2many('fondoda.prestamo','partner_id',string='Prestamos',readonly=True)
 
     @api.onchange('benef_birth')
     def calculate_age(self):
@@ -41,6 +41,30 @@ class FondoContact(models.Model):
                 return {'warning': {
                     'title': "Error",
                     'message': "La fecha de nacimiento no es válida",
+                    }
+                }
+
+    @api.onchange('benef_phone')
+    def onchange_phone_verification(self):
+        if self.benef_phone:
+            telefono = str(self.benef_phone)
+            telefono.replace(" ","")
+            temp=list(filter(lambda numero: numero in '0123456789',telefono) )
+            tam_telefono = len(temp)
+            caracteres = [numero for numero in str(telefono) if not numero in '+-(0123456789)']
+            if caracteres:
+                self.phone=False
+                return {'warning': {
+                    'title': "Error Teléfono",
+                    'message': "No se permite el uso de letras",
+                    }
+                }
+            if tam_telefono >= 10 and tam_telefono <= 15:
+                pass
+            else:
+                return {'warning': {
+                    'title': "Error Teléfono",
+                    'message': "Favor de introducir un numero con al menos 10 caracteres",
                     }
                 }
 
@@ -68,7 +92,6 @@ class FondoContact(models.Model):
                     }
                 }
 
-
     @api.onchange('email')
     def method_validete_mail(self):
         if self.email:
@@ -85,18 +108,30 @@ class FondoContact(models.Model):
     @api.onchange('num_colab')
     def onchange_colab_verification(self):
         if self.num_colab:
-            colab = str(self.num_colab)
+            colab = self.num_colab
             colab.replace(" ","")
             temp=list(filter(lambda numero: numero in '0123456789',colab) )
-            caracteres = [numero for numero in str(colab) if not numero in '0123456789']
-            colaboradores = self.env['res.partner'].search([('num_colab','=',self.num_colab)])
-            if caracteres or len(colaboradores) > 1:
-                self.colab=False
+            colab = len(temp)
+            caracteres = [numero for numero in str(colab) if not numero in '(0123456789)']
+            if caracteres:
+                self.num_colab = False
                 return {'warning': {
                     'title': "Error Colaborador",
-                    'message': "Puede que contenga letras, o el colaborador ya existe",
+                    'message': "El número colaborador contiene caracteres",
                     }
                 }
+            #colab = str(self.num_colab)
+            #colab.replace(" ","")
+            #temp=list(filter(lambda numero: numero in '0123456789',colab) )
+            #caracteres = [numero for numero in str(colab) if not numero in '0123456789']
+            #colaboradores = self.env['res.partner'].search([('num_colab','=',self.num_colab)])
+            #if caracteres or len(colaboradores) > 1:
+            #    self.colab=''
+            #    return {'warning': {
+            #        'title': "Error Colaborador",
+            #        'message': "Puede que contenga letras, o el colaborador ya existe",
+            #        }
+            #    }
     
     @api.depends('user_ids','active')
     def define_type_of_user(self):
@@ -105,3 +140,22 @@ class FondoContact(models.Model):
                 res.is_colaborator = True
             else:
                 res.is_colaborator = False
+
+
+    @api.model
+    def create(self, vals):
+        res = super(FondoContact, self).create(vals)
+        colab = self.env['res.partner'].search([('num_colab','=',vals['num_colab'])])
+        if len(colab) > 1:
+            raise ValidationError(('El número de colaborador que intenta agregar ya existe'))
+        else:
+            return res
+    
+
+    def write(self, vals):
+        res = super(FondoContact, self).write(vals)
+        colab = self.env['res.partner'].search([('num_colab','=',self.num_colab)])
+        if len(colab) > 1:
+            raise ValidationError(('El número de colaborador que intenta agregar ya existe'))
+        else:
+            return res
--- models/prestamo/solicitud_prestamo.py
+++ models/prestamo/solicitud_prestamo.py
@@ -1,11 +1,11 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError,ValidationError
 
 class FondodaPrestamo(models.Model):
     _name = 'fondoda.prestamo'
 
     name = fields.Char('Nombre')
-    partner_id = fields.Many2one('res.partner','Colaborador')
+    partner_id = fields.Many2one('res.partner','Colaborador',default=lambda self: self.env.user.partner_id)
     prestamos_activos = fields.Boolean('¿Tienes prestamos activos?')
     cantidad = fields.Float('Cantidad solicitada',digits=(32, 2))
     cantidad_letra = fields.Char('Cantidad solicitada')
@@ -17,6 +17,49 @@ class FondodaPrestamo(models.Model):
         'Tipo descuento',tracking=True,default='quincena')
     monto = fields.Float('Monto de los descuentos',digits=(32, 2))
     interes = fields.Float('Interés(%)')
+    estatus = fields.Selection([
+        ('solicitud', 'Solicitud'),
+        ('aceptada', 'Aceptada'),
+        ('rechazada','Rechazada'),
+        ('pagado','Pagado')],
+        string='Estatus')
+
+    @api.depends('cantidad','prestamos','pagos','interes')
+    def compute_total_descuento(self):
+        for sp in self:
+            if sp.prestamos_activos == False:
+                total = sp.cantidad + (sp.cantidad/(sp.interes/100))
+                sp.descuento = total/sp.pagos
 
 
+    @api.depends('partner_id','estatus')
+    def verify_prestamos(self):
+        for p in self:
+            if p.partner_id and p.partner_id.prestamos_id:
+                p_value = p.partner_id.prestamos_id.filtered(lambda x: x.estatus == 'aceptada')
+                if p_value:
+                    p.prestamos_activos = True
+                else:
+                    p.prestamos_activos = False
+            else:
+                p.prestamos_activos = False
+
+
+    @api.model
+    def create(self, vals):
+        res = super(FondodaPrestamo, self).create(vals)
+        if res.prestamos_activos == True:
+            raise ValidationError(('Error!! No se puede crear la solicitud, debido a que tiene un prestamo activo'))
+        else:
+            return res
     
+    def write(self, vals):
+        res = super(FondodaPrestamo, self).write(vals)
+        if 'estatus' in vals:
+            if self.estatus == 'aceptada' and self.prestamos_activos == True:
+                raise ValidationError(('Error!! No se puede aceptar la solicitud, debido a que tiene un prestamo activo'))
+            else:
+                return res
+        else:
+            return res
+                
